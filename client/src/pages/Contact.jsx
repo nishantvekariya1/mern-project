c4b68ff724ae17052f0215545177560a01fe94ea
nishantvekariya1 <nishantvekariya623@gmail.com>
2024-01-24 14:10:19 +0530

    autofill contact form

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useAuth } from "../store/auth";
 
 const defaultContactFormData = {
   username: "",
@@ -8,6 +9,19 @@ const defaultContactFormData = {
 
 export default function Contact() {
   const [contact, setContact] = useState(defaultContactFormData);
+
+  const [userData, setUserData] = useState(true);
+
+  const { user } = useAuth();
+
+  if (userData && user) {
+    setContact({
+      username: user.username,
+      email: user.email,
+      message: "",
+    });
+    setUserData(false);
+  }
 
   const handleInput = (e) => {
     let name = e.target.name;
--- client/src/store/auth.jsx
+++ client/src/store/auth.jsx
@@ -1,9 +1,11 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(localStorage.getItem("token"));
+  const [user, setUser] = useState();
+  const authorizationToken = `Bearer ${token}`;
   const storeTokenInLS = (serverToken) => {
     setToken(serverToken);
     return localStorage.setItem("token", serverToken);
@@ -16,8 +18,38 @@ export const AuthProvider = ({ children }) => {
     return localStorage.removeItem("token");
   };
 
+  const userAuthentication = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/api/auth/user", {
+        method: "GET",
+        headers: {
+          Authorization: authorizationToken,
+        },
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setUser(data.userData);
+      }
+    } catch (error) {
+      console.log(`Error in Fetching data from DB : ${error}`);
+    }
+  };
+
+  useEffect(() => {
+    userAuthentication();
+  }, []);
+
   return (
-    <AuthContext.Provider value={{ isLoggedIn, LogoutUser, storeTokenInLS }}>
+    <AuthContext.Provider
+      value={{
+        isLoggedIn,
+        LogoutUser,
+        storeTokenInLS,
+        user,
+        authorizationToken,
+      }}
+    >
       {children}
     </AuthContext.Provider>
   );
