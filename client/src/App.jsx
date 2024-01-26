9b73e6f903637b70e11f0b237dcb4ddcdf2c0fe1
nishantvekariya1 <nishantvekariya623@gmail.com>
2024-01-26 15:55:35 +0530

    implement 404 page functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Error from "./pages/Error";
 import { Logout } from "./pages/Logout";
 
 export default function App() {
@@ -24,6 +25,7 @@ export default function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
           <Route path="/logout" element={<Logout />} />
+          <Route path="*" element={<Error />} />
         </Routes>
         <Footer />
       </BrowserRouter>
