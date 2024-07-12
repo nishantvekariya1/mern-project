ac5c63661a0997db72b2e13e14be2cd84fd25c74
nishantvekariya1 <nishantvekariya623@gmail.com>
2024-07-12 08:45:01 +0530

    some changes done

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -60,80 +60,87 @@ export default function Contact() {
 
   return (
     <>
-      <section className="p-4">
-        <div>
-          <h1 className="text-xl font-bold">Contact us</h1>
-        </div>
-        {/* contact page main  */}
-        <div className="mt-4">
-          {/* <div className="contact-img">
-      <img src="/images/support.png" alt="we are always ready to help" />
-    </div> */}
-
-          {/* contact form content actual  */}
-          <section>
-            <form onSubmit={handleSubmit} className="max-w-md mx-auto">
-              <div className="mb-4">
-                <label htmlFor="username" className="block">
-                  username
-                </label>
-                <input
-                  type="text"
-                  name="username"
-                  id="username"
-                  autoComplete="off"
-                  value={contact.username}
-                  onChange={handleInput}
-                  className="w-full border border-gray-300 rounded px-3 py-2"
-                  required
+      <section>
+        <main>
+          <div className="section-login">
+            <div className="container grid grid-two-cols">
+              <div className="login-image">
+                <img
+                  src="/images/support.png"
+                  alt="we are always ready to help"
+                  height="500"
+                  width="500"
                 />
               </div>
+              <div className="login-form">
+                <h1 className="main-heading mb-3">Contact us</h1>
+                <br />
 
-              <div className="mb-4">
-                <label htmlFor="email" className="block">
-                  email
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  autoComplete="off"
-                  value={contact.email}
-                  onChange={handleInput}
-                  className="w-full border border-gray-300 rounded px-3 py-2"
-                  required
-                />
-              </div>
+                <form onSubmit={handleSubmit} className="max-w-md mx-auto">
+                  <div className="mb-4">
+                    <label htmlFor="username" className="block">
+                      username
+                    </label>
+                    <input
+                      type="text"
+                      name="username"
+                      id="username"
+                      autoComplete="off"
+                      value={contact.username}
+                      onChange={handleInput}
+                      className="w-full border border-gray-300 rounded px-3 py-2"
+                      required
+                    />
+                  </div>
 
-              <div className="mb-4">
-                <label htmlFor="message" className="block">
-                  message
-                </label>
-                <textarea
-                  name="message"
-                  id="message"
-                  autoComplete="off"
-                  value={contact.message}
-                  onChange={handleInput}
-                  className="w-full border border-gray-300 rounded px-3 py-2"
-                  required
-                  cols="30"
-                  rows="6"
-                ></textarea>
-              </div>
+                  <div className="mb-4">
+                    <label htmlFor="email" className="block">
+                      email
+                    </label>
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      autoComplete="off"
+                      value={contact.email}
+                      onChange={handleInput}
+                      className="w-full border border-gray-300 rounded px-3 py-2"
+                      required
+                    />
+                  </div>
 
-              <div>
-                <button
-                  type="submit"
-                  className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
-                >
-                  submit
-                </button>
+                  <div className="mb-4">
+                    <label htmlFor="message" className="block">
+                      message
+                    </label>
+                    <textarea
+                      name="message"
+                      id="message"
+                      autoComplete="off"
+                      value={contact.message}
+                      onChange={handleInput}
+                      className="w-full border border-gray-300 rounded px-3 py-2"
+                      required
+                      cols="30"
+                      rows="6"
+                    ></textarea>
+                  </div>
+
+                  <div>
+                    <button
+                      type="submit"
+                      className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+                    >
+                      submit
+                    </button>
+                  </div>
+                </form>
               </div>
-            </form>
-          </section>
-        </div>
+            </div>
+          </div>
+        </main>
       </section>
+      ;
     </>
   );
 }
